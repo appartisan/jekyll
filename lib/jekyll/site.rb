b7f21d215e6874b41649ada2c022c910fd50d38c
Larry Salibra <larry@appartisan.com>
2012-11-29 17:24:44 +0800

    add includes accessor

--- lib/jekyll/site.rb
+++ lib/jekyll/site.rb
@@ -5,7 +5,7 @@ module Jekyll
   class Site
     attr_accessor :config, :layouts, :posts, :pages, :static_files,
                   :categories, :exclude, :include, :source, :dest, :lsi, :pygments,
-                  :permalink_style, :tags, :time, :future, :safe, :plugins, :limit_posts
+                  :permalink_style, :tags, :time, :future, :safe, :plugins, :limit_posts, :includes
 
     attr_accessor :converters, :generators
 
